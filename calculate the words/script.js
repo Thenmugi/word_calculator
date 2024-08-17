8912b80f62b3178451340ca29fcb9769ba0d9869
Thenmugi <142307003+Thenmugi@users.noreply.github.com>
2024-08-17 10:33:15 +0530

    Add files via upload

---
+++ calculate the words/script.js
@@ -0,0 +1,3 @@
+let para ="a prefix appearing in loanwords from Greek, most often attached to verbs and verbal derivatives, with the meanings “at or to one side of, beside, ..."
+let arr=para.split(" ")
+console.log(arr)
